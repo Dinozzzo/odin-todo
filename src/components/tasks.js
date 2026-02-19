790845bceec9c87c2f13f4ec73ed331c60003b05
Dinozzzo <dleootta@gmail.com>
2026-02-19 17:57:43 +0800

    fixed the last details, add task button is ready. Need to create DIALOG

--- src/components/tasks.js
+++ src/components/tasks.js
@@ -2,13 +2,16 @@ export default function tasks() {
   const container = document.createElement("div");
   container.classList.add("tasks-container");
 
-  // TITLE
+  // TITLE + ADD TASKS
   const title = document.createElement("h3");
   title.textContent = "Tasks";
 
   // SIDEBAR + BUTTONS
   const sideBar = document.createElement("div");
   sideBar.classList.add("tasks-side-bar");
+
+  const leftSidebar = document.createElement("div");
+  leftSidebar.classList.add("left-sidebar");
 
   const allBtn = document.createElement("button");
   allBtn.textContent = "All";
@@ -21,10 +24,14 @@ export default function tasks() {
   const deletedBtn = document.createElement("button");
   deletedBtn.textContent = "Deleted";
 
-  // ADD A TASK
+  // CREATE TASK BUTTON
+  const addTaskBtn = document.createElement("button");
+  addTaskBtn.textContent = "+ Create a new task";
+  addTaskBtn.classList.add("addtask-btn");
 
   // ASSEMBLAGE
-  sideBar.append(allBtn, overdueBtn, upcomingBtn, completedBtn, deletedBtn);
+  leftSidebar.append(allBtn, overdueBtn, upcomingBtn, completedBtn, deletedBtn);
+  sideBar.append(leftSidebar, addTaskBtn);
   container.append(title, sideBar);
   return container;
 }
